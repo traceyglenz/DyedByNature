a2dca28b71415ab01e41cebcac2ea3f4ecadb2b3
Eli King <elikingrealestate@gmail.com>
2019-05-09 19:48:14 -0400

    20190609 nav links, modal submit, social icons

--- js/master.js
+++ js/master.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
 		$('#myModal').modal('show');
 	});
+
+	$('#fire').on('click', function (e) {
+	            alert("Thank you for requesting a newsletter!");
+	            });
