cd43036727c8e438b805b25808c87369c5abc3fa
traceyglenz <traceyglenz@gmail.com>
2019-05-19 16:09:00 -0400

    modal appears part way down the page

--- js/master.js
+++ js/master.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
-		$('#myModal').modal('show');
-	});
+// $(document).ready(function(){
+// 		$('#myModal').modal('show');
+// 	});
+
+$(window).scroll(function() {
+       if ($(document).scrollTop() > 3000 && $("#myModal").attr("displayed") === "false") {
+         $('#myModal').modal('show');
+         $("#myModal").attr("displayed", "true");
+       }
+     });
 
 	$('#fire').on('click', function (e) {
 							$(".modal-title").text("Thank you!");
-							$(".modal-body").text("Check your inbox for our newsletter.");
+							$(".modal-body").text("Check your inbox for our newsletter. Have a great day!");
 							$(".modal-footer").text("");
 							});
